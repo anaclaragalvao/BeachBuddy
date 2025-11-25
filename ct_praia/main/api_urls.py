0efaa30bf322f4e37c1ce386a5e546a4dfd93b77
Ana Clara Galvao <clara.galvao@hotmail.com.br>
2025-11-25 08:42:54 -0300

    fix: removido endpoints não utilizados

--- ct_praia/main/api_urls.py
+++ ct_praia/main/api_urls.py
@@ -1,10 +1,6 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView,
-)
+
 
 from . import api_views
 
@@ -16,10 +12,7 @@ router.register(r'inscricoes', api_views.InscricaoViewSet, basename='inscricao')
 router.register(r'usuarios', api_views.UsuarioViewSet, basename='usuario')
 
 urlpatterns = [
-    # Autenticação JWT
-    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('auth/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
     
     # Cadastro e login personalizado
     path('auth/signup/', api_views.signup_view, name='api_signup'),
